ec1875d177eb23233f764ce80bf949009864bec0
Sasa Sekaric <sasasekaric@hotmail.com>
2016-03-25 16:40:01 +0100

    Not allow user_id in permited params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -97,6 +97,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end
